01981a8892191a2f26a9ca1b390ef0188c3190d3
Julio Aguilar <julio.aguilar@bunsan.io>
2021-01-13 22:41:47 -0600

    🚀 axios module was configured globally

--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@ import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import store from './store'
+import axios from 'axios'
 
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './plugins/vue-router'
 
 Vue.use(BootstrapVue)
+
+Vue.use({
+  install(Vue) {
+    Vue.prototype.$axios = axios.create({
+      baseURL: 'https://api.github.com'
+    })
+  }
+})
 
 Vue.config.productionTip = false
 
